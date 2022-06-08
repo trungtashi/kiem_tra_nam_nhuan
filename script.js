925a75fa84bdd073b20ebc2b74a60dccc0a8b203
trungtashi99 <trungtashi@gmai.com>
2022-06-08 10:31:01 +0700

    Thực hành kiểm tra năm nhuận

---
+++ script.js
@@ -0,0 +1,24 @@
+let year = parseInt(prompt('Nhập năm'))
+let leapYear = false
+
+let div4 = year%4 == 0
+if(div4){
+    let div100 = year % 100 == 0
+    if(div100){
+        let div400 = year%400 == 0
+        if(div400){
+            leapYear = true
+        }
+    }
+    else
+    {
+        leapYear = true
+    }
+}
+if(leapYear == true){
+    alert(year + ' là năm nhuận')
+}
+else
+{
+    alert(year + ' không phải năm nhuận')
+}
